42bfbd7700fe464a1c662dc4d439b370e14573e8
Stanislav Khromov <stanislav@khromov.se>
2023-07-27 00:21:47 +0200

    WIP

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,10 +1,8 @@
-import * as db from '$lib/server/database';
-
 /** @type {import('./$types').PageServerLoad} */
-export async function load({ params }) {¨
-    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-    const posts = await response.json();
-        
+export async function load({ params }) {
+    //const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    //const posts = await response.json();
+    const posts = [];
     return {
         posts
     };
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,8 @@
 <h1>Blog posts</h1>
 
+<!--
 {#each posts as post}
     <h2>{post.title}</h2>
     <a href="/posts/{post.id}">Read more</a>
 {/each}
+-->
